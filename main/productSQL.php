090d9aa185ac765efaafe56e2d10824780bb5f73
EscapeFromJava <reske.artur@gmail.com>
2022-06-16 19:01:35 +0300

    v 4.5 clear code

--- main/productSQL.php
+++ main/productSQL.php
@@ -9,13 +9,12 @@ $result = mysqli_query($link, $sql);
 
 $arProducts = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
-foreach ($arProducts as $key => $value) {
-    $currentID = $arProducts[$key]['ID'];
-    $currentName = $arProducts[$key]['NAME'];
-    $currentShortDescription = $arProducts[$key]['SHORT_DESCRIPTION'];
-    $currentSpecifications = $arProducts[$key]['SPECIFICATIONS'];
-    $currentDescription = $arProducts[$key]['DESCRIPTION'];
-    $currentImage = $arProducts[$key]['IMAGE'];
-    $currentPrice = $arProducts[$key]['PRICE'];
-  }
+$currentID = $arProducts[$key]['ID'];
+$currentName = $arProducts[$key]['NAME'];
+$currentShortDescription = $arProducts[$key]['SHORT_DESCRIPTION'];
+$currentSpecifications = $arProducts[$key]['SPECIFICATIONS'];
+$currentDescription = $arProducts[$key]['DESCRIPTION'];
+$currentImage = $arProducts[$key]['IMAGE'];
+$currentPrice = $arProducts[$key]['PRICE'];
+  
 ?>
